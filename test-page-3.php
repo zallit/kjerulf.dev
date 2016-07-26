25783a358bdeb49d673e4eb9ec32954bc5dc2724
Andreas Kjerulf <andreaskjerulf@gmail.com>
2016-07-26 15:59:50 +0200

    testing new dropdown menu

--- test-page-3.php
+++ test-page-3.php
@@ -1,23 +1,43 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <link href="style.css" rel="stylesheet" type="text/css">
-  <meta charset="utf-8">
+<style media="screen">
+body{ overflow-y: scroll; margin: 0; padding: 0; font-family: Karla; }
+
+nav{ background-color: #383332; text-align: center; }
+nav ul{ list-style-type: none; padding: 0; margin: 0; position: relative; }
+nav ul li{ display: inline-block; }
+nav ul li:hover{ background-color: #2465F3; }
+nav ul li a{ display: block; padding: 15px; color: #FFF; text-decoration: none; }
+
+nav ul li:hover > ul{ display: block; }
+nav ul ul{ padding: 3px 0 3px 0; display: none; text-align: left; position: absolute; background-color: #2465F3; border: 2px solid #383332; border-top: 0; margin: 0 0 0 -2px; min-width: 200px; }
+nav ul ul li{ display: block; border: none; }
+nav ul ul li:hover{ background-color: #383332; }
+
+@import url(https://fonts.googleapis.com/css?family=Karla);
+
+</style>
 <title>Test page 3</title>
 
 </head>
 <h1>Test page 3</h1>
 <nav>
-      <?php
-        $TITLE = "test page 3";
-        ob_start();
-      ?>
 
-      <?php
 
-        $CONTENT = ob_get_clean();
-        include "template.html"
-      ?>
+  </font>
+
+  <ul>
+      <li><a href="#">Home</a></li>
+      <li><a href="#">Changelog</a></li>
+      <li><a href="#">Dropdown</a>
+      <ul>
+        <li><a href="test-page-1.php">Test page 1 (respnsesive menu and old site)</a></li>
+        <li><a href="test-page-2.php">Test page 2</a></li>
+        <li><a href="test-page-3.php">Test page 3</a></li>
+      </ul>
+      </li>
+      </ul>
 </nav>
 
 <body>
