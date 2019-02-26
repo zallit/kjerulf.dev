4f0af84d757bcfc8aac4f6cf2a689cbb7c0f3360
Zallit <andreaskjerulf@gmail.com>
2019-02-26 14:47:04 +0100

    started work on the tools page

---
+++ tools.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <link href="style.css" rel="stylesheet" type="text/css">
+    <meta charset="utf-8">
+  <title>Tools used to code Zallit´s testing site </title>
+  </head>
+<body>
+      <h1 id="top">Tools used to code Zallit´s testing site</h1>
+  <nav>
+        <?php
+          $TITLE = "Changelog";
+          ob_start();
+        ?>
+
+        <?php
+
+          $CONTENT = ob_get_clean();
+          include "template.html";
+        ?>
+  </nav>
+
+  <h2>Editor</h2>
+    <h4> Github´s ATOM <a href=www.atom.io>www.atom.io</a>   </h4> 
+      <h5>Plugins</h5>
+
+
+
+
+GIT <br>
+  Github <br>
+
+
+</body>
