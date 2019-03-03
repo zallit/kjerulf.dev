f91438b3c9387852a460a5deba28c6094675e0b9
Zallit <andreaskjerulf@gmail.com>
2019-03-03 19:42:27 +0100

    working on the layout in the tools page

--- tools.php
+++ tools.php
@@ -9,7 +9,7 @@
       <h1 id="top">Tools used to code Zallit´s testing site</h1>
   <nav>
         <?php
-          $TITLE = "Changelog";
+          $TITLE = "Tools";
           ob_start();
         ?>
 
@@ -19,16 +19,14 @@
           include "template.html";
         ?>
   </nav>
-
-  <h2>Editor</h2>
-    <h4> Github´s ATOM <a href=www.atom.io>www.atom.io</a> with the following plugins <br> </h4>
-    <h5><li>emmet <a href="https://atom.io/packages/emmet">  </li>
-  <li>git-plus https://github.com/akonwi/git-plus</li>
-  <li>remote-FTP https://github.com/icetee/remote-ftp </li> </h5>
-  <br>
-
-<h2>Code storage</h2> <br>
-  Github <br>
-  AZE webhosting <a href=https://my.azehosting.net/aff.php?aff=286>www.aze-hosting.dk
+<h1>Editor</h1>
+<a href=www.atom.io>Github´s ATOM Editor </a> with the following plugins: <br>
+<a href=https://atom.io/packages/emmet>Emmet</a> <br>
+<a href=https://github.com/akonwi/git-plus>git-plus </a> <br>
+<a href=https://github.com/icetee/remote-ftp>remote-FTP</a> <br>
+<br>
+<h1>Code storage</h1> <br>
+<h2>Github </h2> <br>
+<h2>AZE webhosting <a href=https://my.azehosting.net/aff.php?aff=286>www.aze-hosting.dk </h2>
 
 </body>
