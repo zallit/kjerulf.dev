39da6f637fab3cb37cd76e96d6bb52d21bd91af4
Zallit <andreaskjerulf@gmail.com>
2019-03-04 17:16:18 +0100

    still working on the tools page and introduced a bug

--- tools.php
+++ tools.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <link href="style.css" rel="stylesheet" type="text/css">
+    <!--<link href="style.css" rel="stylesheet" type="text/css">
     <meta charset="utf-8">
   <title>Tools used to code Zallit´s testing site </title>
   </head>
@@ -18,15 +18,16 @@
           $CONTENT = ob_get_clean();
           include "template.html";
         ?>
-  </nav>
-<h1>Editor</h1>
-<a href=www.atom.io>Github´s ATOM Editor </a> with the following plugins: <br>
-<a href=https://atom.io/packages/emmet>Emmet</a> <br>
-<a href=https://github.com/akonwi/git-plus>git-plus </a> <br>
-<a href=https://github.com/icetee/remote-ftp>remote-FTP</a> <br>
-<br>
-<h1>Code storage</h1> <br>
-<h2>Github </h2> <br>
-<h2>AZE webhosting <a href=https://my.azehosting.net/aff.php?aff=286>www.aze-hosting.dk </h2>
+  </nav> -->
+***stylesheet temporary off due to a bug***
+<h1> <u>Editor</u></h1>
+<h2> <a href=www.atom.io>Github´s ATOM Editor </a> with the following plugins: </h2>
+<h3> <ul style="list-style-type:disc;"> <li> <a href=https://atom.io/packages/emmet>Emmet</a>   </li>
+<li> <a href=https://github.com/akonwi/git-plus>git-plus </a>  </li>
+<li> <a href=https://github.com/icetee/remote-ftp>remote-FTP</a>  </li>  </ul></h3> 
+
+<h1> <u>Code storage</u></h1>
+<h2> <a href=https://github.com/zallit/Zallit.dk-code</a> Github  <br>
+<a href=https://my.azehosting.net/aff.php?aff=286> Webserver </h2>
 
 </body>
