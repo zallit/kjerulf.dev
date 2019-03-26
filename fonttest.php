e4182d567e1b6288f739912d723a4985b076aa1d
Zallit <andreaskjerulf@gmail.com>
2019-03-26 15:29:44 +0100

    experimenting with fonts

---
+++ fonttest.php
@@ -0,0 +1,22 @@
+<head>
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans|Roboto" rel="stylesheet">
+  <link href="style.css" rel="stylesheet" type="text/css">
+  <meta charset="utf-8">
+<title>Font test</title>
+
+</head>
+<body>
+<p>Lorem ipsum dolor sit amet porttitor. Ultricies sit sed. Urna et tincidunt tincidunt orci in. Vel viverra ullamcorper sem ut in ligula arcu sed. Eu tincidunt ac phasellus id mi ante mollis quisque. At aliquam massa pede fringilla mauris vel eu pede tristique massa tortor. Duis ac pulvinar. Proin metus dui. Tristique sed penatibus. Nulla aliquam ut fringilla tristique per.</p>
+<p>Mollis mollis sem. Nulla quam curabitur. Odio amet sed ante quis egestas. Per saepe semper tellus mus lorem. Vivamus metus tortor. Velit et con. Ac tellus eget enim feugiat et. Pellentesque pede con. Mauris a blandit vestibulum nunc neque tristique lectus magna. Rutrum euismod dui. Et sed dolor at dui accumsan ultrices pellentesque neque. Orci enim in. Elit ut et at arcu a. Quis ultricies velit distinctio lorem dolor. Urna suspendisse pede eum eros suscipit ultricies molestie ac. Nulla felis quis sed hendrerit diam dictum voluptate felis pellentesque mi arcu est tellus hac est urna dis metus mus interdum eu pellentesque libero. Ac sunt mauris. Maecenas id orci. Id id dictum id amet vulputate. Eget morbi turpis nunc proin nulla.</p>
+<p>Est volutpat neque. Nulla ut enim nullam ante id. Vitae vestibulum lorem non quisque potenti. Commodo natoque nec. Magna aenean suscipit sapien odit massa. Ac magna bibendum. Commodo convallis tristique convallis nonummy faucibus ullamcorper at faucibus velit at nulla aenean et commodi. Pellentesque ut curabitur vitae sed praesent. Est fames id. Sed ac elit eros lectus aenean. Dignissim arcu wisi elit mattis donec. Varius nunc etiam. Leo lorem eget. Urna pede dolor amet magna pellentesque enim in accumsan. Sed turpis vitae. Dui ultricies consequat aspernatur dui elit risus cursus id. Curabitur donec a porta enim sollicitudin. Vel inceptos quam. Eget fusce et dui faucibus orci curabitur suspendisse sociis facilisis eget mauris leo pretium condimentum quisque lacinia pharetra. Tristique venenatis etiam. </p>
+<h1>Lorem ipsum dolor sit amet porttitor. Ultricies sit sed. Urna et tincidunt tincidunt orci</h1>
+<br>
+<h2>Lorem ipsum dolor sit amet porttitor. Ultricies sit sed. Urna et tincidunt tincidunt orci</h2>
+<br>
+<h3>Lorem ipsum dolor sit amet porttitor. Ultricies sit sed. Urna et tincidunt tincidunt orci</h3>
+<br>
+<h4>Lorem ipsum dolor sit amet porttitor. Ultricies sit sed. Urna et tincidunt tincidunt orci</h4>
+
+Lorem ipsum dolor sit amet porttitor. Ultricies sit sed. Urna et tincidunt tincidunt orci
+
+</body>
