df98c2c86587b5a3b69c8f7e03d359170452d855
Zallit <andreaskjerulf@gmail.com>
2019-08-05 18:45:55 +0200

    updated links

--- start/index.php
+++ start/index.php
@@ -22,21 +22,21 @@
       ?>
   </nav>
 <br>
-<a href="https://my.azehosting.net/" > Aze admin panel </a>
+<a href="https://my.azehosting.net/aff.php?aff=286" target="_blank"> Aze admin panel </a>
 <br><br>
-<a href="https://bddatabase.net"> Black desert database </a>
-<br> <br>
-<a href="https://bdoplanner.com/"> BDO gear planner </a>
-<br> <br>
-<a href="https://bdoplanner.com/AzwellCollegeScale"> BDO my Shai </a>
-<br> <br>
-<a href="https://grumpygreen.cricket/bdo-failstack-chart/BDO"> Failstack guide </a>
+<a href="https://bddatabase.net" target="_blank"> Black desert database </a>
 <br><br>
-<a href="https://kjerulf.dev/trss/"> RSS reader </a>
+<a href="https://bdoplanner.com/" target="_blank"> BDO gear planner </a>
 <br><br>
-<a href="https://kjerulf.dev/l/"> URL shortner </a>
+<a href="https://bdoplanner.com/AzwellCollegeScale" target="_blank"> BDO my Shai </a>
 <br><br>
-<a href="https://zallit.dk/smf" >So much fun forum </a>
+<a href="https://grumpygreen.cricket/bdo-failstack-chart/BDO" target="_blank"> Failstack guide </a>
+<br><br>
+<a href="https://kjerulf.dev/trss/" target="_blank"> RSS reader </a>
+<br><br>
+<a href="https://kjerulf.dev/l/" target="_blank"> URL shortner </a>
+<br><br>
+<a href="https://zallit.dk/smf" target="_blank"> So much fun forum </a>
 
 
 </body>
